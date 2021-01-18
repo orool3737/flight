8f629d64f3cf8adc04b0d26f24091087d5cefb33
orool3737 <orool3737@naver.com>
2021-01-18 06:12:51 +0000

    Update cralwer.py

--- cralwer.py
+++ cralwer.py
@@ -12,7 +12,8 @@ url = "http://www.slrclub.com/"
 options = webdriver.ChromeOptions()
 options.add_argument('headless')    # 웹 브라우저를 띄우지 않는 headlss chrome 옵션 적용
 options.add_argument('disable-gpu')    # GPU 사용 안함
-driver = webdriver.Chrome("./chromedriver", options=options)
+chrome_driver = os.path.join('chromedriver')
+driver = webdriver.Chrome(chrome_driver, options=options)
 
 driver.get(url)
 time.sleep(1)
