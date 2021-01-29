29f79fbf5c7ee554d63ff15e9e425d247b0b4675
orool3737 <orool3737@naver.com>
2021-01-29 17:00:29 +0900

    Update flight.py

--- flight.py
+++ flight.py
@@ -68,16 +68,15 @@ department = soup.select("dd.txt_code.ng-binding")
 price = soup.select("span.txt_pay.ng-binding")
 
 dateformat = "%H:%M"
-print(department_time)
-'''
+
 i=0
 while soup:
     try:
-        print(company[i].text, department[i].text, department_time[i].text, price[i].text)
+        print(company[i].text, department[i].text, department_time[3*i].text, price[i].text)
         i = i + 1
     except IndexError:
         break
-'''
+
 '''
 elem = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul")))
 #-> elem는 웹드라이버를 통해 브라우져에서 최대 10초를 기다려주고 xpath 기준으로 값에 해당하는 elem가 나올때까지 기다려줘.
