fd1c5a9fedb94bb0cf81e430cd032374d118ff66
orool3737 <orool3737@naver.com>
2021-01-30 10:42:28 +0900

    Update flight.py

--- flight.py
+++ flight.py
@@ -84,7 +84,7 @@ price = soup.select("span.txt_pay.ng-binding")
 i=0
 while soup:
     try:
-        department_hour = datetime.strptime(department_time[3*i].text, '%H:%M').hour
+        department_hour = datetime.datetime.strptime(department_time[3*i].text, '%H:%M').hour
         if department_hour >= 18 and department_hour <= 19:
            print(company[i].text, department[i].text, department_time[3*i].text, price[i].text)
         i = i + 1
