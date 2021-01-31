bb788c94913c3b3c0d49bd3afaafb2bee99d6822
orool3737 <orool3737@naver.com>
2021-02-01 00:14:09 +0900

    Update flight.py

--- flight.py
+++ flight.py
@@ -55,7 +55,7 @@ third_content.insert(0,friday[3].strftime('%Y-%m-%d'))
 total_content = first_content + second_content + third_content
 total_content_clean = '\n'.join(total_content)
 print(total_content_clean)
-
+ 
 bot = telegram.Bot(token='1676490998:AAGXQvSDIZl-W9-yDnWh7WNKLHqdGQ9zOw4')
 chat_id = 1491027495 #bot.getUpdates()[-1].message.chat.id
 
