6e907f2d5a1fd102c405b3e578781d2b253cc569
orool3737 <orool3737@naver.com>
2021-01-31 22:10:15 +0900

    Update cralwer.py

--- cralwer.py
+++ cralwer.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def exract_flight(flight_day, flight_month);
+def exract_flight(flight_day, flight_month):
    url='https://flight.naver.com/flights/'
 
    options = webdriver.ChromeOptions()
