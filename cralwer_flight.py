4ade3c68eccf6c3208b70b0c6a380b4b710e3330
orool3737 <orool3737@naver.com>
2021-02-01 11:28:16 +0900

    Update cralwer_flight.py

--- cralwer_flight.py
+++ cralwer_flight.py
@@ -42,8 +42,9 @@ def exract_flight(flight_day, flight_month):
 
    #항공권 검색 클릭
    driver.find_element_by_link_text("항공권 검색").click()
-
-   WebDriverWait(driver, 180).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
+   
+   driver.implicitly_wait(180)
+   #WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
 
    # 스크롤 가장 아래로 내리기
    interval = 2
